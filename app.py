7b40e192ab800ac11674574e08c2facbe5af36fb
Bethanasamy Rajamani <46414051+Bethana86@users.noreply.github.com>
2024-02-24 19:26:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -37,3 +37,11 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True) 
